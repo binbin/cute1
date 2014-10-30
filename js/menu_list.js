f32f8f239110dd28bddeafacc9054f544c7c3020
binbin <binbin5474@gmail.com>
2014-10-30 12:25:22 +0800

    menu

--- js/menu_list.js
+++ js/menu_list.js
@@ -1,10 +1,17 @@
 $(function(){
-	$('ul.list > li > span > a').click(function(){
-       var ul =$(this).parent().find('ul')
+	$('span.menu_item > a').click(function(){
+       var ul =$(this).parent().parent().find('ul')
+       var border_fix=ul.next()
+       var ul_p_box=ul.parent()
        if(ul.is(':visible')){
-       	 ul.slideUp()
+       	 ul.slideUp(function(){
+       	 	ul_p_box.removeClass('open')
+       	 	
+       	 })
        }else{
-       	 ul.slideDown()
+       	 ul.slideDown(function(){
+			ul_p_box.addClass('open')
+       	 })
        }
 	})
 })
